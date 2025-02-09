c0aff4651563c9eedddeeb8e61e5e1aa9963c722
Edward3957503 <edwardpadmajaya@gmail.com>
2025-02-09 21:44:39 +1100

    Added addBlog page

--- next-blog-app/app/api/blog/route.js
+++ next-blog-app/app/api/blog/route.js
@@ -1,4 +1,5 @@
 import { ConnectDB } from "@/lib/config/db";
+import BlogModel from "@/lib/models/BlogModel";
 const { NextResponse } = require("next/server");
 import {writeFile} from 'fs/promises';
 
@@ -28,6 +29,7 @@ export async function POST(request) {
         author: `${formData.get('author')}`,
         image: `${imgUrl}`,
         authorImg: `${formData.get('authorImg')}`,
+        date: new Date()
     }
 
     await BlogModel.create(blogData);
