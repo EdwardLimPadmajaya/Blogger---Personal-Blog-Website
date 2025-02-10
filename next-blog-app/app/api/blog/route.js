081ffa0b0f9b0146730fbfcde6431c1d6143dae0
Edward3957503 <edwardpadmajaya@gmail.com>
2025-02-10 20:40:27 +1100

    Added BlogList dashboard features for admin

--- next-blog-app/app/api/blog/route.js
+++ next-blog-app/app/api/blog/route.js
@@ -2,6 +2,7 @@ import { ConnectDB } from "@/lib/config/db";
 import BlogModel from "@/lib/models/BlogModel";
 const { NextResponse } = require("next/server");
 import {writeFile} from 'fs/promises';
+const fs = require('fs');
 
 const LoadDB = async () => {
     await ConnectDB();
@@ -43,4 +44,13 @@ export async function POST(request) {
     await BlogModel.create(blogData);
     console.log("Blog Saved");
     return NextResponse.json({success:true, msg: "Blog Added"});
+}
+
+// API Endpoint For Deleting Blog
+export async function DELETE(request) {
+    const id = await request.nextUrl.searchParams.get('id');
+    const blog = await BlogModel.findById(id);
+    fs.unlink(`./public${blog.image}`, () => {});
+    await BlogModel.findByIdAndDelete(id);
+    return NextResponse.json({success:true, msg: "Blog Deleted"});
 }
